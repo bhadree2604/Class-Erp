ce9b4d25234aa6ab935ba17cc85fd5cd49c4cccd
Bhadree07456 <bhadree.rs@gmail.com>
2026-08-29 16:27:04 +0530

    updates

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:mockito/mockito.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:rit_erp/main.dart';
+import 'package:rit_erp/services/auth_service.dart';
 import 'package:rit_erp/screens/login_screen.dart';
+
+class MockFirebaseFirestore extends Mock implements FirebaseFirestore {}
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  // Mock Firebase services
+  AuthService.authOverride = MockFirebaseAuth();
+  AuthService.firestoreOverride = MockFirebaseFirestore();
 
   testWidgets('Landing screen shows login elements', (WidgetTester tester) async {
     await tester.pumpWidget(MyClassApp(startPage: const LoginScreen(), key: myAppKey));
